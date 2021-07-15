85db1930c13817f6d841e7900a30735944a9edcd
Thomas Castleman <contact@draugeros.org>
2021-07-14 22:05:14 -0400

    bug fixes for deploy and uninstall

--- setup.sh
+++ setup.sh
@@ -2,13 +2,16 @@
 echo "Installing Dependencies . . ."
 sudo apt install --assume-yes $(<requirements.txt)
 username=$(whoami)
-loc="$PWD"
 echo "Configuring your system . . ."
 sudo cp -v flask-app.nginx_conf /etc/nginx/sites-available/flask-app.conf
-sudo ln -v /etc/nginx/sites-available/flask-app.conf /etc/nginx/sites-enabled/flask-app.conf
 sudo cp -v download_optimizer.service /etc/systemd/system/download_optimizer.service
-sudo sed -i "s/<path to>/$loc/g" /etc/nginx/sites-available/flask-app.conf
-sudo sed -i "s/<path to>/$loc/g" /etc/systemd/system/download_optimizer.service
-sudo sed -i "s/<username>/$username/g" /etc/systemd/system/download_optimizer.service
+sudo sed -i "s:<path to>:$PWD:g" /etc/nginx/sites-available/flask-app.conf
+sudo sed -i "s:<path to>:$PWD:g" /etc/systemd/system/download_optimizer.service
+sudo sed -i "s:<username>:$username:g" /etc/systemd/system/download_optimizer.service
+echo "Disabling default site . . ."
+sudo rm -fv /etc/nginx/sites-enabled/default
+echo "Enabling site and restarting Nginx . . ."
 sudo systemctl enable download_optimizer
+sudo ln -sv /etc/nginx/sites-available/flask-app.conf /etc/nginx/sites-enabled/flask-app.conf
+sudo systemctl restart nginx
 echo "Please open port 80 so that Download Optimizer may be exposed to the network"
--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-echo "Removing files . . ."
+echo "Removing files and disabling. . ."
+sudo systemctl disable download_optimizer
 sudo rm -fv /etc/nginx/sites-available/flask-app.conf /etc/nginx/sites-enabled/flask-app.conf /etc/systemd/system/download_optimizer.service
+sudo systemctl restart nginx
