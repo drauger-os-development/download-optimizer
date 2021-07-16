3f22c1f58b0160089a11b7331f2be9fd4c7310fc
Thomas Castleman <contact@draugeros.org>
2021-07-15 22:51:33 -0400

    add ability to configure which port is used

--- setup.sh
+++ setup.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
+port="$1"
+if [ "$port" == "" ]; then
+	port="80"
+fi
 echo "Installing Dependencies . . ."
 sudo apt install --assume-yes $(<requirements.txt)
 username=$(whoami)
 echo "Configuring your system . . ."
-sudo cp -v flask-app.nginx_conf /etc/nginx/sites-available/flask-app.conf
+sudo cp -v download_optimizer.nginx_conf /etc/nginx/sites-available/download_optimizer.conf
 sudo cp -v download_optimizer.service /etc/systemd/system/download_optimizer.service
-sudo sed -i "s:<path to>:$PWD:g" /etc/nginx/sites-available/flask-app.conf
+sudo sed -i "s:<path to>:$PWD:g" /etc/nginx/sites-available/download_optimizer.conf
+sudo sed -i "s:<port>:$port:g" /etc/nginx/sites-available/download_optimizer.conf
 sudo sed -i "s:<path to>:$PWD:g" /etc/systemd/system/download_optimizer.service
 sudo sed -i "s:<username>:$username:g" /etc/systemd/system/download_optimizer.service
 echo "Disabling default site . . ."
 sudo rm -fv /etc/nginx/sites-enabled/default
 echo "Enabling site and restarting Nginx . . ."
 sudo systemctl enable download_optimizer
-sudo ln -sv /etc/nginx/sites-available/flask-app.conf /etc/nginx/sites-enabled/flask-app.conf
+sudo ln -sv /etc/nginx/sites-available/download_optimizer.conf /etc/nginx/sites-enabled/download_optimizer.conf
 sudo systemctl restart nginx
 echo "Please open port 80 so that Download Optimizer may be exposed to the network"
--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Removing files and disabling. . ."
 sudo systemctl disable download_optimizer
-sudo rm -fv /etc/nginx/sites-available/flask-app.conf /etc/nginx/sites-enabled/flask-app.conf /etc/systemd/system/download_optimizer.service
+sudo rm -fv /etc/nginx/sites-available/download_optimizer.conf /etc/nginx/sites-enabled/download_optimizer.conf /etc/systemd/system/download_optimizer.service
 sudo systemctl restart nginx
