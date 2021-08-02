c2fca58490f940ccc5b21357c5d4a1dd481d8226
Thomas Castleman <contact@draugeros.org>
2021-08-01 22:11:15 -0400

    stop WSGI server if uninstalling

--- uninstall.sh
+++ uninstall.sh
@@ -22,6 +22,7 @@
 #
 #
 echo "Removing files and disabling. . ."
+sudo systemctl stop download_optimizer
 sudo systemctl disable download_optimizer
 sudo rm -fv /etc/nginx/sites-available/download_optimizer.conf /etc/nginx/sites-enabled/download_optimizer.conf /etc/systemd/system/download_optimizer.service
 sudo systemctl restart nginx
